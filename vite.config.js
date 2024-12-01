b38cda996bda3826b7c9aa1a5725a4ca5a645c86
kundlatsch <gustavo.kundlatsch@gmail.com>
2024-12-01 13:50:08 -0300

    Deploy working

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,18 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/honeycomb-software/',
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
+    }
+  },
+  css: {
+    devSourcemap: true
+  }
 })
